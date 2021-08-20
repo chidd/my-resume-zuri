37993f2988870b99646a2f60c2872fec62f16272
chinedu <chi.adams54@gmail.com>
2021-08-20 19:27:50 +0100

    my resume nodejs website initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,68 @@
+/*!
+* Start Bootstrap - Resume v7.0.3 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+const contactForm = document.querySelector('#contactForm');
+let name = document.getElementById('name')
+let subject = document.getElementById('subject')
+let email = document.getElementById('email')
+let message = document.getElementById('message')
+
+contactForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let formData = {
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    }
+    let xhr =  new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success'){
+            alert('Email sent');
+            name.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        }else{
+            alert('Something went wrong');
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+})
+
---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+dotenv.config({path:'config.env'});
+const PORT = process.env.PORT || 8080;
+
+//log requests
+app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+// set view engine
+app.set("view engine", "ejs")
+// app.set("views", path.resolve(__dirname,"views/ejs"))
+
+//MIDDLEWARE
+app.use(express.json()); //to recieve formdata
+// load assets
+app.use("/assets", express.static(__dirname + "/assets"));
+// app.use('/css', express.static(path.resolve(__dirname,"assets/css")))
+// app.use('/img', express.static(path.resolve(__dirname,"assets/img")))
+// app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
+// app.use(express.static("assets"));
+
+
+app.get('/', (req,res)=>{
+    res.render('index');
+})
+
+app.post('/',(req, res)=>{
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: 'chinedu@geekict.com.ng',
+            pass: 'Kvu@2kd@[H;l'
+        }
+    })
+
+    const mailOptions  = {
+        from: req.body.nodemailer,
+        to: 'chinedu@geekict.com.ng',
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions,(error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email sent: ' + info.response);
+            res.send('Success');
+        }
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`)
+})
